1b3c08b974c879b35a247f0fa73680ebaf0e58ad
dankernel <google@dankook.ac.kr>
2015-12-11 02:13:53 +0900

    auto-20151211-021353

--- 700/main.c
+++ 700/main.c
@@ -18,14 +18,75 @@
 #include <stdlib.h>
 #include <stdio.h>
 
-void f_701(void)
+void f_703(void)
 {
+  int y, x;
 
+  for(y = 0; y < 7; y++){
+    for(x = 0; x < (6 - y); x++)
+      printf(" ");
+    for(x = 6 - y; x < 7; x++)
+      printf("*");
+    printf("\n");
+  }
+}
+
+void f_705(void)
+{
+  int input,
+      ret;
+
+  printf("Input Arg : 1 ");
+  ret = scanf("%d", &input);
+
+  while(input)
+    printf("%d ", input--);
+
+  printf("\n");
+}
+
+
+void f_707(void)
+{
+  int tmp = 0,
+      i = 0,
+      j = 0;
+
+  for (i = 2; i < 100; i++) {
+    j = 2;
+    while (i % j++ != 0 && j < i);
+
+    if (i <= j)
+      printf("%d, ", i);
+  }
+}
+
+void f_709(void)
+{
+  int n = 10000;
+
+  printf("%d\n", n * (n + 1) / 2);
+}
+
+void f_711(void)
+{
+  int input,
+      ret,
+      add,
+      i;
+
+  printf("Input Arg1 (int)\n");
+  ret = scanf("%d", &input);
+
+  for (i = 1; i <= input; i++) {
+    add += i * i;
+  }
+  printf("%d \n", add);
 }
 
 int main(int argc, const char *argv[])
 {
-  
+  f_711();
 
   return 0;
 }
