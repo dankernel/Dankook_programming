10b63522fd0aed5ce9bdb24defc6396ac67eaed9
dankernel <google@dankook.ac.kr>
2015-12-11 01:20:09 +0900

    auto-20151211-012009

---
+++ 700/main.c
@@ -0,0 +1,32 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  main.c
+ *
+ *    Description:  
+ *
+ *        Version:  1.0
+ *        Created:  2015년 12월 10일 22시 16분 03초
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  YOUR NAME (), 
+ *   Organization:  
+ *
+ * =====================================================================================
+ */
+#include <stdlib.h>
+#include <stdio.h>
+
+void f_701(void)
+{
+
+}
+
+int main(int argc, const char *argv[])
+{
+  
+
+  return 0;
+}
+
